bf665d3f8b9c805689a0566e0c8715c0ea5e795e
wsrogers <wsrogers@users.noreply.github.com>
2019-11-18 11:30:45 -0500

    added a place to put cards and a comment for referencing once temp gets replaced

---
+++ engine/Cards.js
@@ -0,0 +1,22 @@
+/*
+    id: the id of the card
+    name: the name of the card
+    cardimg: the display image for the card
+    attack: how much damage the card does
+    defense: the health points of the card before the card is destroyed
+    cost: how much mana the card costs
+    ability: the special ability a card has if it has one
+    abilityNumber: the ID of the given ability
+ */
+
+const cardData=[{
+    id:0,
+    name:"temp",
+    cardimg: "link here",
+    attack:0,
+    defense:0,
+    cost:0,
+    ability:"temp",
+    abilityNumber:0,
+     }
+]
