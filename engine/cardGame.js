5227620a8dbb96c3719e3738d29c7acfeca9d22e
W. Alan Patterson II <walanpat@live.unc.edu>
2019-12-03 11:20:42 -0500

    ojiajiogjioag

--- engine/cardGame.js
+++ engine/cardGame.js
@@ -1,5 +1,28 @@
-//lets get this bread (i messed up the commit)
+export default class cardGame {
+    //So we start off with nothing, and intake a deck
+    //The deck we intake is our own,  Mind you
+    //I've written this so that it's initializing
+    //1 side of the game, not the AI side of the game
+    //Meaning that this is just how you play the game, There will be interactions with the opponents board
+    //I just need to code them.
+    constructor(p1deck, p2deck) {
+        this.p1TurnCounter = 1;
+        this.p1hp = 100;
+        this.p1Hand = new Array[7];
+        this.p1Board = new Array[7].fill(0);
+        this.p1Deck = new Array[40].fill(0);
 
+        this.p2TurnCounter = 1;
+        this.p2hp = 100;
+        this.p2Hand = new Array[7];
+        this.p2Board = new Array[7].fill(0);
+        this.p2Deck = new Array[40].fill(0);
+        for (let i = 0; i < 40; i++) {
+            this.p1Deck[i] = p1deck[i];
+            this.p2Deck[i] = p2deck[i];
+        }
+    }
+}
 let aiDeck=cardData;
 let playerDeck =cardData;
 aiDeck =shuffle(aiDeck);
