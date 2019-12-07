5081410ac76e9be904158316869662d88d5e4773
Marigrace Seaton <marigraceseaton@gmail.com>
2019-12-07 02:49:36 -0500

    added some functions to render, am open to changing them

--- Render.js
+++ Render.js
@@ -1,22 +1,31 @@
-import axios from 'axios';
-
-const root = new axios.create({
-    baseURL: "http://localhost:3000"
-});
-  
-export const handleLogin = async function(e) {
-    e.preventDefault(); 
-
-//    const result = await root.post(`/account/login/`, {
-//      'name': user, 
-//      'pass': pass,
-//    })
-
-//    console.log(result); 
+function signIn() {
+    // Sign into Firebase using popup auth & Google as the identity provider.
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
 }
 
-$(async function() {
+function signOut() {
+    // Sign out of Firebase.
+    firebase.auth().signOut();
+}
 
-    $(document).on('click', '#loginSubmit', console.log('login submit')); 
+function initFirebaseAuth() {
+    // Listen to auth state changes.
+    // Lets us ~coders~ know if the user is signed in or signed out, so we can change the UI depending on that 
+    firebase.auth().onAuthStateChanged(authStateObserver);
+}
 
-})
+// Returns the signed-in user's profile pic URL.
+function getProfilePicUrl() {
+    return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
+}
+  
+// Returns the signed-in user's display name.
+function getUserName() {
+    return firebase.auth().currentUser.displayName;
+}
+
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+    return !!firebase.auth().currentUser;
+}
