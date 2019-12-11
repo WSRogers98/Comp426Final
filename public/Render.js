e49d163f467e6beb922334b60baa8243a136a544
Marigrace Seaton <marigraceseaton@gmail.com>
2019-12-10 21:16:35 -0500

    this is what i have  (#22)
    * Added given Backend from professors
    
    * All I did was add the backend provided to us by the prof.
    
    * i hope this works
    
    * ojiajiogjioag
    
    * added render function, still not quite working
    
    * removed login form from hero div
    
    * still trying to get login submit button to log into console teehee just comp sci things
    
    * i've made no progress! ugh!
    
    * deleted type field from script tag sourcing render
    
    * with firebase
    
    * update index.html
    
    * another update
    
    * added some functions to render, am open to changing them
    
    * i am a firebase fangirl
    
    * changed quotation mark type in index
    
    * penis
    
    * we have some styling yall
    
    * okay let's get this goddamn game working
    
    * WE HAVE WORKING LOGIN AND CREATING ACCOUNTS BABYYYYYY
    
    * GOOGLE BUTTON IS SHOWING UP
    
    * centered sign in with google button
    
    * added a reset password page
    
    * idk what changed in these files
    
    * now you can reset your password if you forgot it
    
    * added back to home button because i couldn't figure out the continue url thing
    
    * Delete app.js
    
    idk why this still pulls up

--- public/Render.js
+++ public/Render.js
@@ -1,5 +1,121 @@
 import { cardGame } from "./engine/cardGame.js";
 import { cardData } from "./engine/Cards.js";
+// assigns the authorization app to an easily typed variable bc im lazy
+let auth = firebase.auth(); 
+
+// handles login button press 
+function toggleSignIn() {
+    // if user is logged in already, logs them out 
+    if (auth.currentUser) {
+        auth.signOut(); 
+    } else {
+        // gets email and password from submitted form 
+        let email = document.getElementById('email').value; 
+        let password = document.getElementById('password').value; 
+        auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+            // handles sign in errors here 
+            let errorCode = error.code; 
+            let errorMessage = error.message; 
+            if (errorCode === 'auth/wrong-password') {
+                alert('Wrong password.'); 
+            } else {
+                alert(errorMessage); 
+            }
+            console.log(error); 
+        })
+    }
+}
+
+// handles sign up button press 
+function handleSignUp() {
+    let email = document.getElementById('email').value; 
+    let password = document.getElementById('password').value; 
+    // creates user with email and password gathered above 
+    auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // handles error here 
+        let errorCode = error.code; 
+        let errorMessage = error.message; 
+        if (errorCode === 'auth/weak-password') {
+            alert('The password is too weak.'); 
+        } else {
+            alert(errorMessage); 
+        }
+        console.log(error); 
+    })
+    toggleSignIn(); 
+}
+
+// handles logging in with google 
+function toggleSignInWithGoogle() {
+    // if person isn't already logged in 
+    if (!auth.currentUser) {
+        let provider = new firebase.auth.GoogleAuthProvider(); 
+        // signs user in 
+        auth.signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API. 
+            let token = result.credential.accessToken; 
+            let user = result.user; 
+        }).catch(function(error) {
+            // handles errors 
+            let errorCode = error.code; 
+            let errorMessage = error.message; 
+            // The provider account's email address 
+            let email = error.email; 
+            // the pending google credential 
+            let credential = error.credential; 
+            if (errorCode === 'auth/account-exists-with-different-credential') {
+                alert('You have already signed up with a different auth provider for that email.'); 
+                // hande linking user accounts signed up with multiple auth providers here 
+                // User's email already exists.
+                // Asks the user their password.
+                var password = promptUserForPassword(); // TODO: implement promptUserForPassword.
+                auth.signInWithEmailAndPassword(email, password).then(function(user) {
+                    user.linkWithCredential(credential);
+                });
+            } else {
+                console.log(error); 
+            }
+        });
+    } else {
+        auth.signOut(); 
+    }
+}
+
+// renders login with google button *******************************************************************************************
+    function onSuccess(googleUser) {
+      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    }
+    function onFailure(error) {
+      console.log(error);
+    }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
+  // ******************************************************************************************************************************8
+
+function handleResetEmail() {
+    let emailAddress = document.getElementById('resetEmail').value;
+
+    auth.sendPasswordResetEmail(emailAddress).then(function() {
+        // email sent 
+    }).catch(function(error) {
+        // handle errors here
+        alert(error.message); 
+    });
+}
+
+
+
+
+
 
 
 
@@ -158,6 +274,11 @@ $(function () {
     })
     $(document).on('click', '#wiki', function () {wikipage();})
 
+  
+    $(document).on('click', '#loginSubmit', toggleSignIn); 
+    $(document).on('click', '#createAccount', handleSignUp);  
+    $(document).on('click', '#my-login2', toggleSignInWithGoogle); 
+    $(document).on('submit', '#resetPassword', handleResetEmail); 
     //Templates for xon clicks of cards and various items, need changes later ~~~~~Don't change the one above
     $(document).on('click', '#playerhand-0', function () { cardPlay(0, true); })
     $(document).on('click', '#playerhand-1', function () { cardPlay(1, true); })
